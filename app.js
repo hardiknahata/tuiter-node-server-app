78cf752d688368264cc9a713f35b3b931459afc6
Hardik Nahata <hardiknahata@gmail.com>
2022-11-26 19:04:05 -0500

    Mongo Connection String

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 import HelloController
   from "./controllers/hello-controller.js"
 import UserController
